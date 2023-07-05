b3755a8881932bebeb9fd636cdc91699614ede89
Bharathboy1 <86122926+Bharathboy1@users.noreply.github.com>
2023-07-05 18:29:52 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -12,7 +12,7 @@ myclient = pymongo.MongoClient(DATABASE_URI)
 db = myclient[DATABASE_NAME]
 col = db[COLLECTION_NAME]
 
-last_sent_message = {"_id": 0}  
+last_sent_message_id = 0
 
 
 def get_last_sent_message_from_database(channel_id):
