6774ec3ed68c61931ae6c6cae25ac1ab0edd28db
Bharathboy1 <86122926+Bharathboy1@users.noreply.github.com>
2023-07-05 20:51:35 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -4,6 +4,7 @@ from info import DATABASE_URI, DATABASE_NAME, COLLECTION_NAME, ADMINS, CHANNELS,
 from database.ia_filterdb import save_file
 import asyncio
 import random
+from utils import get_size
 
 media_filter = filters.document | filters.video | filters.audio
 
@@ -68,12 +69,12 @@ async def x(app, msg):
        
         try:
             try:
-                media = await app.get_media(i['id'])
-                file_size = media.get_file_size()
-                await app.send_video(msg.chat.id, i['id'], caption=CUSTOM_FILE_CAPTION.format(file_name=i['file_name'], file_caption=i['file_caption'], file_size=file_size))
+                #media = await app.get_media(i['id'])
+                #file_size = media.get_file_size()
+                await app.send_video(msg.chat.id, i['id'], caption=CUSTOM_FILE_CAPTION.format(file_name=i['file_name'], file_caption=i['file_caption'], file_size=get_size(int(i['file_size'))
             except Exception as e:
                 print(e)
-                await app.send_document(msg.chat.id, i['id'], caption=CUSTOM_FILE_CAPTION.format(file_name=i['file_name'], file_caption=i['file_caption'], file_size=file_size))
+                await app.send_document(msg.chat.id, i['id'], caption=CUSTOM_FILE_CAPTION.format(file_name=i['file_name'], file_caption=i['file_caption'], file_size=get_size(int(i['file_size'))
             await jj.edit(f"Found {len(id_list)} Files In The DB Starting To Send In Chat {args}\nProcessed: {j+1}")
             col.update_one({'_id': 'last_msg'}, {'$set': {'index': j}}, upsert=True)
             await asyncio.sleep(random.randint(8, 10))
